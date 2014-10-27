1301f843176671d7029ba86aa254913366aed905
Zoltán Vörös <zvoros@gmail.com>
2014-10-27 10:16:22 +0100

    added main.css and modes.js

--- web/static/js/functions.js
+++ web/static/js/functions.js
@@ -1,4 +1,15 @@
-var server_address = 'http://127.0.0.1:8080'
+$(document).ready(function () {
+	$('.colourpicker').colpick({
+		colorScheme:'dark',
+		layout:'rgbhex',
+		color:'ff8800',
+		onSubmit:function(hsb,hex,rgb,el) {
+			$(el).css('background-color', '#'+hex)
+			submit_colour(el, rgb)
+			$(el).colpickHide()
+		}
+	}).css('background-color', '#ff8800')
+});
 
 function command_keypress(event) {
 	console.log($('#input_command').val())	
---
+++ web/static/js/modes.js
@@ -0,0 +1,11 @@
+var server_address = 'http://127.0.0.1:8080'
+
+//var modes = {
+//{'Link 1', '/mode1.'}, 
+//{'Link 2', '/mode2.'}
+//}
+
+$(document).ready(function () {
+	$('aside').append('<ul id="ul_modes"></ul>')
+	$('#ul_modes').append('<li>blabla</li>')
+});
