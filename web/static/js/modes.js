f23846870e45d976d18db7192c1b4135f62e80e2
Zoltán Vörös <zvoros@gmail.com>
2014-10-27 10:49:29 +0100

    updated modes.js functions

--- web/static/js/modes.js
+++ web/static/js/modes.js
@@ -1,11 +1,13 @@
 var server_address = 'http://127.0.0.1:8080'
 
-//var modes = {
-//{'Link 1', '/mode1.'}, 
-//{'Link 2', '/mode2.'}
-//}
+var modes = new Array(
+['Colour picker, 6 lights', '/colourpicker.mode'], 
+['Stroboscope', '/stroboscope.mode']
+)
 
 $(document).ready(function () {
 	$('aside').append('<ul id="ul_modes"></ul>')
-	$('#ul_modes').append('<li>blabla</li>')
+	for(var i in modes) {
+		$('#ul_modes').append('<li><a href=?mode=' + server_address + modes[i][1] + '>' + modes[i][0] + '</a></li>')
+	}
 });
